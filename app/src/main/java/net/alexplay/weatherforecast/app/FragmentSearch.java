4a739fc6a35dc3fcdbb6f4e2b5ff473c61710d10
websters_dog <tumarnik@yandex.ru>
2015-02-13 01:10:50 +0000

    Initial commit

---
+++ app/src/main/java/net/alexplay/weatherforecast/app/FragmentSearch.java
@@ -0,0 +1,25 @@
+package net.alexplay.weatherforecast.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FragmentSearch extends Fragment {
+
+
+
+    public FragmentSearch() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.search_fragment, container, false);
+        return rootView;
+    }
+}
